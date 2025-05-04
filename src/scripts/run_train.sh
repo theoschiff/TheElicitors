35b44465a57fe121a1b0ed274020bf175f90cfa6
jeremmmyyyyy <jeremy.barghorn@epfl.ch>
2025-05-04 11:34:15 +0200

    Updated scripts

--- src/scripts/run_train.sh
+++ src/scripts/run_train.sh
@@ -12,7 +12,7 @@
 
 echo STARTING AT `date`
 
-module load gcc cuda  openmpi python hdf5/1.14.3-mpi  py-scipy
+module load gcc cuda openmpi python
 
 cd ..
 cd MasterProject/
@@ -26,23 +26,23 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-GPUS_PER_NODE=2
-# so processes know who to talk to
-MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
-MASTER_PORT=6000
-NNODES=$SLURM_NNODES
-NODE_RANK=$SLURM_PROCID 
-WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
-NODELIST=($(scontrol show hostnames $SLURM_JOB_NODELIST))
-TRAIN_NODES=("${NODELIST[@]}")
+# GPUS_PER_NODE=2
+# # so processes know who to talk to
+# MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+# MASTER_PORT=6000
+# NNODES=$SLURM_NNODES
+# NODE_RANK=$SLURM_PROCID 
+# WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+# NODELIST=($(scontrol show hostnames $SLURM_JOB_NODELIST))
+# TRAIN_NODES=("${NODELIST[@]}")
 
 
-echo "MASTER_ADDR=$MASTER_ADDR"
-echo "MASTER_PORT=$MASTER_PORT"
-echo "NNODES=$NNODES"
-echo "NODE_RANK=$NODE_RANK"
-echo "WORLD_SIZE=$WORLD_SIZE"
-echo "TRAIN_NODES=$TRAIN_NODES"
+# echo "MASTER_ADDR=$MASTER_ADDR"
+# echo "MASTER_PORT=$MASTER_PORT"
+# echo "NNODES=$NNODES"
+# echo "NODE_RANK=$NODE_RANK"
+# echo "WORLD_SIZE=$WORLD_SIZE"
+# echo "TRAIN_NODES=$TRAIN_NODES"
 
 export TOKENIZERS_PARALLELISM=true
 
@@ -50,4 +50,4 @@ export HF_HUB_ENABLE_HF_TRANSFER=1
 
 export HF_HOME="/scratch/barghorn/.cache"
 
-srun accelerate launch --num_processes 1 --config_file configs/deepspeed_zero3.yaml train/rule_based_grpo.py --config receipes/rule_based_grpo.yaml
+accelerate launch --num_processes 1 --config_file configs/deepspeed_zero3.yaml train/rule_based_grpo.py --config reciepes/rule_based_grpo.yaml
