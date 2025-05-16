45ab3a64c7360d4d7e0fe0fc4c7b6fec4365fec3
lindsaybordier <lindsayski@gmail.com>
2025-05-16 14:07:34 +0200

    Prepare to run (change directory to find configs file)

--- src/scripts/run_train_lindsay.sh
+++ src/scripts/run_train_lindsay.sh
@@ -41,10 +41,11 @@ sleep 120
 echo "Starting GRPO training"
 
 # Get in correct src directory
-cd ..
+cd
+cd TheElicitors/src
 
 # Stage 2: GRPO Training
 export CUDA_VISIBLE_DEVICES=1
 ACCELERATE_LOG_LEVEL=info \
 accelerate launch --config_file configs/deepspeed_zero3.yaml --num_processes 2 \
-    train/rule_based_grpo.py --config reciepes/rule_based_grpo.yaml
+    train/rule_based_grpo.py --config receipes/rule_based_grpo.yaml
