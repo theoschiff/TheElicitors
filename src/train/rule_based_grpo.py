716b34bac3921966544f3def95a13d3ad53d1be7
jeremmmyyyyy <jeremy.barghorn@epfl.ch>
2025-05-20 23:39:41 +0200

    Fixed reward computations name missmatch for poetry

--- src/train/rule_based_grpo.py
+++ src/train/rule_based_grpo.py
@@ -125,8 +125,7 @@ def grpo_function(
     elif script_args.task_type == "poetry":
         sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
         print(f"Sentence_model on device: {sentence_model.device}")
-        similarity_reward = partial(sentence_similarity_reward_func, sentence_model=sentence_model)
-        similarity_reward.__name__ = "sentence_similarity_reward_func"
+        similarity_reward_with_norm = partial(similarity_reward_with_norm, sentence_model=sentence_model)
         reward_functions = [format_reward_with_norm, similarity_reward_with_norm, reward_poem_form_with_norm, rhyme_accuracy_with_norm, syllable_accuracy_with_norm]
         training_args.reward_weights = [0.1, 0.25, 0.25, 0.1, 0.30]
 
